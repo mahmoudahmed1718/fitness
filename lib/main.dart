12670fa1ed9d08611a339ce1ade17b75f0b2a808
mahmoud ahmed <mahmoudahmedhsn@gmail>
2026-02-19 22:24:31 +0200

    add firebase core package and configuration in main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:fitness/features/home/ui/view/home_view.dart';
+import 'package:fitness/firebase_options.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
